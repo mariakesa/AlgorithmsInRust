c5a422caeedfd73bee7b222d05c249222b5ae80a
mariakesa <maria.kesa@gmail.com>
2023-01-31 02:07:40 +0100

    practice

--- main.rs
+++ main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter number!");
+    let mut a=String::new();
+
+    io::stdin()
+        .read_line(&mut a)
+        .expect("Failed to read line");
+
+    println!("A is: {a}");
 }
